ccc3c3447c750c6ac3fc0d9842ec548bdb21f243
mossi <mossi.hashemi@gmail.com>
2018-12-15 19:55:33 +0100

    Add fire base to Ashop

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDOi76NQh5F25eubGkUNFWLFaHMe6fLxe0",
+    authDomain: "ashop-dbf42.firebaseapp.com",
+    databaseURL: "https://ashop-dbf42.firebaseio.com",
+    projectId: "ashop-dbf42",
+    storageBucket: "ashop-dbf42.appspot.com",
+    messagingSenderId: "112669692885"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyDOi76NQh5F25eubGkUNFWLFaHMe6fLxe0",
+    authDomain: "ashop-dbf42.firebaseapp.com",
+    databaseURL: "https://ashop-dbf42.firebaseio.com",
+    projectId: "ashop-dbf42",
+    storageBucket: "ashop-dbf42.appspot.com",
+    messagingSenderId: "112669692885"
+  }
 };
 
 /*
